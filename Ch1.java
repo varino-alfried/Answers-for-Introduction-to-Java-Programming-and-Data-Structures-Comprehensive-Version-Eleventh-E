0338d7cf5e2e0116b72ee22c64b23a6140643922
Varino Fahmy <66516413+varino-alfried@users.noreply.github.com>
2020-12-05 13:54:57 +0200

    Update Ch1.java

--- Ch1.java
+++ Ch1.java
@@ -1,4 +1,16 @@
+/*
+Q1.5 (Compute expressions) Write a program that displays the result of
+
+                        7.5 * 6.5 - 4.5 * 3
+                     ______________________
+                           47.5 - 5.5
+
+*/
 package ch1;
+/**
+ *
+ * @author varino
+ */
 public class Ch1
 {
     public static void main(String[] args)
